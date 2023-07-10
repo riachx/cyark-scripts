cb85082eb9a27af1c66a94ea8d53b07dcdd80e7a
Ria <67889319+riachx@users.noreply.github.com>
2023-07-10 00:24:16 -0700

    minor changes

--- HousekeepingScript/SetVariables.bat
+++ HousekeepingScript/SetVariables.bat
@@ -5,7 +5,6 @@
 
 set /p "DRIVELETTER=Enter the drive of where your project folder is located (e.g., C,D,X): "
 set /p "PROJECTNAME=Enter the project name (e.g., AMX_American_Embassy, QMP_Queen_Museum_Panorama_05_2022): "
-set /p "SCENENAME=Enter a name for your Reality Capture scene (e.g., AMX_American_Embassy_Scene): "
 
 echo:
 echo Ensure the following paths are correct:
@@ -13,17 +12,17 @@ echo:
 
 :: path to RealityCapture application
 REM change to C once finished testing
-set RealityCaptureExe=C:\RealityCapture\RealityCapture.exe
+set RealityCaptureExe="D:\RealityCapture\RealityCapture.exe"
 echo The path to your application is %RealityCaptureExe%
 
 :: root path to work folders where all the datasets are stored (%~dp0 means the flder in which this script is stored)
-set RootFolder=%DRIVELETTER%:\%PROJECTNAME%\Applications\RealityCapture
+set RootFolder="%DRIVELETTER%:\%PROJECTNAME%\Applications\RealityCapture"
 echo The path to your root folder is %RootFolder%
 
 :: set path to the folder with your saved scene
-set Project=%RootFolder%\Scene\%SCENENAME%
+set Project="%DRIVELETTER%:\%PROJECTNAME%\Applications\RealityCapture\Scene\%PROJECTNAME%_Scene.rcproj"
 echo Your scene will be saved at %Project%
 
 :: sets the path of the imported alignment settings (should be where script is saved)
-set AlignmentSettings=%CD%\RC_Alignment_Low_Overlap.xml
+set AlignmentSettings="%CD%\RC_Alignment_Low_Overlap.xml"
 echo Your alignment settings are at %AlignmentSettings%
--- HousekeepingScript/_ProcessAll.bat
+++ HousekeepingScript/_ProcessAll.bat
@@ -1,4 +1,4 @@
-:CapturingReality
+::CapturingReality
 
 :: switch off console output
 @echo off
@@ -18,8 +18,8 @@ echo  *******************************************************
 echo  *                 Housekeeping Script                 *
 echo  *                                                     *
 echo. *  Please read the README.txt file before proceeding! *
-echo  *  This script will save alignment settings, pull a   *
-echo  *  license,                                           *
+echo  *  This script will load global settings, save a      *
+echo  *  template project, clear cache, and quit.           *
 echo  *                                                     *
 echo. *  You will be asked what drive to use and what your  *
 echo. *  project is named in order to maintain proper       *
@@ -43,18 +43,5 @@ if /i "%EMAILBOOL%" == "Y" (
     echo Email is "%EMAILNAME%"
 )
 
-:: use the instance name if you do not want to save project and instead you want to run the open RC.exe
-:: this might be useful in cases where you only want to open RC once, and also not save the project at all.
-:: -setInstanceName "RC_Clean_Template"^
-
-%RealityCaptureExe% -newScene^ 
-    -set "appQuitOnError=true"^
-    -importGlobalSettings "Global_Settings.rcconfig"^
-    -save %Project%^
-    -clearCache 
-    -quit
-
-pause
-
-::if /i "%EMAIL%" == "Y" ()
-:: maybe a loop method of doing realitycapturexe then -load scene and save this scene per loop. save scene above loop too.
+%RealityCaptureExe% -newScene -save %Project% -clearCache -quit
+        
